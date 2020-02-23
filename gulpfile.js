92a028cf32eeb17b761c618b1138dda45a285811
v2538n <53378376+v2538n@users.noreply.github.com>
2020-02-23 11:01:41 +0300

    initial

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp 		 = require('gulp'), 
+	sass 		 = require('gulp-sass'),
+	browserSync  = require('browser-sync');
+
+gulp.task('sass', function(){
+	return gulp.src('app/sass/**/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function(){
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false
+	});
+});
+
+gulp.task('watch', ['browser-sync'], function(){
+	gulp.watch('app/sass/**/*.scss', ['sass']);
+	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/js/**/.js', browserSync.reload);
+});
+
+gulp.task('default', ['watch']);
